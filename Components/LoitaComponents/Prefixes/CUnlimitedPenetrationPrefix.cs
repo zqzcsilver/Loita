aabfb1ca1cc3b086285b27a1c1dc6105cfe5fdcd
ReishyouSose <1657188940@qq.com>
2023-12-12 12:58:09 +0800

    无限穿透修饰

---
+++ Components/LoitaComponents/Prefixes/CUnlimitedPenetrationPrefix.cs
@@ -0,0 +1,24 @@
+﻿namespace Loita.Components.LoitaComponents.Prefixes
+{
+    internal class CUnlimitedPenetrationPrefix : CPrefix
+    {
+        public override string Name => "无限穿透";
+
+        public override string Description => "使你的法术变为无限穿透";
+
+        public CUnlimitedPenetrationPrefix(IEntity entity) : base(entity)
+        {
+        }
+
+        public override void Apply(SpellInfo info)
+        {
+            info.PostShoot += Info_PostShoot;
+            base.Apply(info);
+        }
+
+        private void Info_PostShoot(Terraria.Projectile projectile, IEntity entity)
+        {
+            projectile.penetrate = -1;
+        }
+    }
+}
